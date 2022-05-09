a2893270db704feb738ff2e4fa73a4b8f6e73184
Lars Kecker <lars.kecker2009@gmail.com>
2022-05-10 00:21:37 +0200

    add instructions modal

--- src/index.js
+++ src/index.js
@@ -28,10 +28,30 @@ function clearButtonClicked() {
     clearGuess()
 }
 
+function closeModal() {
+    document.body.classList.remove("modal-open")
+    const activeModals = document.getElementsByClassName("active-modal");
+    for (let modal of activeModals) {
+        modal.classList.remove("active-modal");
+    }
+}
+
+function openModal(modalId) {
+    document.body.classList.add("modal-open")
+    // close all other modals
+    const activeModals = document.getElementsByClassName("active-modal");
+    for (let modal of activeModals) {
+        modal.classList.remove("active-modal");
+    }
+    document.getElementById(modalId).classList.add("active-modal");
+}
+
 init();
 window.actions = {
     randomMoveButtonClicked,
     undoButtonClicked,
     submitButtonClicked,
     clearButtonClicked,
+    closeModal,
+    openModal,
 }
