0eb2924c9901b69c6e8b8b4246a4b4c45595dfdd
Lars Kecker <lars.kecker2009@gmail.com>
2022-05-11 14:21:34 +0200

    remove link between styles and defaults

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import "./defaults.scss";
+import "./style.scss";
+
 import { init as inputInit } from './cube/cubeInput';
 import { init as outputInit } from './cube/cubeOutput';
 import { init as initGame } from './game/game';
 import { clearButtonClicked, randomMoveButtonClicked, submitButtonClicked, undoButtonClicked } from './game/uiInput';
 import { closeModal, showInstructions, showWinScreen } from './game/uiOutput';
-import "./style.scss";
 
 function init() {
     inputInit();
