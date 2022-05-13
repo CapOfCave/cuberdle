6510efc183362f4995668b51cfa137570b078af2
Lars Kecker <lars.kecker2009@gmail.com>
2022-05-13 18:13:25 +0200

    remove babel from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -43,11 +43,11 @@ const config = {
                 test: /\.js$/,
                 loader: "source-map-loader"
             },
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
+            // {
+            //     test: /\.(js)$/,
+            //     exclude: /node_modules/,
+            //     use: ['babel-loader']
+            // },
             {
                 test: /\.pug$/,
                 use: ['pug-loader']
